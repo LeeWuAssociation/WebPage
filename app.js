0c31c5ff493af8da1a6341fbd2a03f15fe9f6883
LeeWuAssociation <61287433+LeeWuAssociation@users.noreply.github.com>
2020-10-18 16:53:36 +0800

    check in new file

---
+++ app.js
@@ -0,0 +1,8 @@
+function area() {
+    var a = parseInt(document.getElementById("a").value);
+    var b = TODO
+    var c = TODO
+    var d = document.getElementById("d");
+    d.value = (a+b)*c/2;
+    alert(TODO);
+} 
